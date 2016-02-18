3569e17e7dc1700c22af4a3e37ad147b6af5a44e
daherr <daherr18@saintjosephhs.com>
2016-02-18 10:31:30 -0500

    Second Commit
    List things i need to do and first lines.

--- ScienceResearchGerryMandeering.java
+++ ScienceResearchGerryMandeering.java
@@ -13,7 +13,21 @@ public class ScienceResearchGerryMandeering {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// test comment to see if github was created right
+
+		/*
+		 * Enter name of a county
+		 * find name of all voting districts / voting areas using queries
+		 * find results from elections online in said voting districts / township using queries
+		 * find GPS coordinates for said voting districts /  townships using queries
+		 * Use distance formula to find the the shortest line between GPS coordinates 
+		 * Draw a map on window to show township /  voting district boundaries
+		 * ***NOTE*** MUST exclude state, city, and preferably unaffected boundaries
+		 */
+		
+		System.out.println("Welcome to the County redistricting program!!!" );
+		
+		System.out.print( "Please enter the name of county you would like to redistrict: " );
+		
 	}
 
 }
