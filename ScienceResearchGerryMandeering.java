6398229d1112901d859e130c65f82b2fb488a284
daherr <daherr18@saintjosephhs.com>
2016-02-22 08:37:02 -0500

    Worked added scanner

--- ScienceResearchGerryMandeering.java
+++ ScienceResearchGerryMandeering.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -15,7 +17,7 @@ public class ScienceResearchGerryMandeering {
 		// TODO Auto-generated method stub
 
 		/*
-		 * Enter name of a county
+		 * Enter name of a county- check
 		 * find name of all voting districts / voting areas using queries
 		 * find results from elections online in said voting districts / township using queries
 		 * find GPS coordinates for said voting districts /  townships using queries
@@ -23,10 +25,12 @@ public class ScienceResearchGerryMandeering {
 		 * Draw a map on window to show township /  voting district boundaries
 		 * ***NOTE*** MUST exclude state, city, and preferably unaffected boundaries
 		 */
-		
+		Scanner keyboard = new Scanner(System.in);
+		String countyName = null;
 		System.out.println("Welcome to the County redistricting program!!!" );
 		
 		System.out.print( "Please enter the name of county you would like to redistrict: " );
+		countyName = keyboard.nextLine();
 		
 	}
 
