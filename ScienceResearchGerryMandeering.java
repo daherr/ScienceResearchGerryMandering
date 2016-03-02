5182d5f9ab41d7059400b3475aa4199d8f2857b1
daherr <daherr18@saintjosephhs.com>
2016-03-02 15:25:13 -0500

    Made a few changes needs work, lots of work

--- ScienceResearchGerryMandeering.java
+++ ScienceResearchGerryMandeering.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 
 /**
  * 
@@ -8,16 +12,56 @@ import java.util.Scanner;
  * @author SJHSStudent
  *
  */
-public class ScienceResearchGerryMandeering {
+public class ScienceResearchGerryMandeering implements ActionListener {
 
 	/**
 	 * @param args
 	 */
+	private static final long serialVersionUID = 1; 
+	private GridBagLayout layout;
+	private JTextField myTextField; // variable for text box
+	private JFrame("MainFrame");
+	
+	/**
+	 * @param args
+	 */
+	public ScienceResearchGerryMandeering(){
+		
+		 super( "ScienceResearchGerryMandeering");
+		 layout = new GridBagLayout();
+		 // variable for the gridbag layout
+		 JButton button;
+		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 GridBagConstraints c = new GridBagConstraints(); // creates a new GridBagRestraint
+			// general constraints
+			c.fill = GridBagConstraints.BOTH; // sets c to resize components BOTH vertically and horizontally
+			// text field setup
+		
+			
+			button = new JButton("Redictrict");
+			button.setFont(new Font("TimesRoman", Font.ITALIC, 12)); // sets font, makes font italicized and font size
+			button.addActionListener(this); // adds ActionListener to button to roll dice
+			layout.setConstraints(button, c); // more button constraints
+			add(button); // adds button to frame
+
+			myTextField = new JTextField("Please enter a county you would like to redistrict"); // creates new text field and sets text in textfield
+			c.weightx = 1.0; // sets weight for text field to have more weight
+			c.gridwidth = 5; // allocates 5 cells for text field
+			c.gridwidth = GridBagConstraints.REMAINDER; // text field restraints
+			layout.setConstraints(myTextField, c); // text field constraints
+			myTextField.setEditable(true); // makes the text field not editable 
+			add(myTextField);
+			 
+			setResizable(false); // makes the frame unresizable 
+			JFrame.setExtendedState( JFrame.MAXIMIZED_BOTH ); // sets the frame to the preferred size 
+			 setVisible(true); // makes the frame visible
+		 
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new ScienceResearchGerryMandeering();
 		/*
-		 * Enter name of a county- check
+		 * Enter name of a county
 		 * find name of all voting districts / voting areas using queries
 		 * find results from elections online in said voting districts / township using queries
 		 * find GPS coordinates for said voting districts /  townships using queries
@@ -25,12 +69,11 @@ public class ScienceResearchGerryMandeering {
 		 * Draw a map on window to show township /  voting district boundaries
 		 * ***NOTE*** MUST exclude state, city, and preferably unaffected boundaries
 		 */
-		Scanner keyboard = new Scanner(System.in);
-		String countyName = null;
-		System.out.println("Welcome to the County redistricting program!!!" );
 		
-		System.out.print( "Please enter the name of county you would like to redistrict: " );
-		countyName = keyboard.nextLine();
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 
